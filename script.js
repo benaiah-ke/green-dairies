4af67017dbcc5da13ddbc0946b904ebf686416ab
benaiah-ke <benaiahpraise@gmail.com>
2022-07-17 11:02:26 +0300

    get variables from html form

---
+++ script.js
@@ -0,0 +1,6 @@
+let proceed = function () {
+    var shedA = document.getElementById("shed-a").value;
+    var shedB = document.getElementById("shed-b").value;
+    var shedC = document.getElementById("shed-c").value;
+    var shedD = document.getElementById("shed-d").value;
+}
