b8be17c70be5f4cfe15f3f170ebe05f435722976
benaiah-ke <benaiahpraise@gmail.com>
2022-07-17 12:23:25 +0300

    Minor changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+var price = 45;
+
 let proceed = function () {
     var shedA = document.getElementById("shed-a").value;
+    
     var shedB = document.getElementById("shed-b").value;
     var shedC = document.getElementById("shed-c").value;
     var shedD = document.getElementById("shed-d").value;
+
+    document.getElementById("a-daily-p").innerHTML = shedA + " " + "litres";
 }
